60e40ed70236a06827d62aeea3da3791d3ece8ac
bhimantoro <bhimantoros@gmail.com>
2018-03-11 10:23:04 +0700

    minggu 10:22

--- bab 3/studi kasus 2.py
+++ bab 3/studi kasus 2.py
@@ -1,7 +1,11 @@
-import pprint
+'''import pprint
 kata = raw_input('Masukkan kata : ')
 count = {}
 for huruf in kata:
     count.setdefault(huruf, 0)
     count[huruf] = count[huruf] + 1
-pprint.pprint(count)
+pprint.pprint(count)'''
+
+import pprint
+s=str(raw_input('Masukkan kata : '))
+pprint.pprint({x:list(s).count(x) for x in list(s)})
